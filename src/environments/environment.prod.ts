b97dec2d1ad1d3fad561944aa3032eb663afc536
Kamalesh Sah <kamalesh.sah07@gmail.com>
2024-05-15 22:52:33 +0530

    Production Build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBAYQgfLF2ieuswCQy7nXdzvCShZNUDBEQ",
+    authDomain: "indiastat-f4ac0.firebaseapp.com",
+    projectId: "indiastat-f4ac0",
+    storageBucket: "indiastat-f4ac0.appspot.com",
+    messagingSenderId: "1074770307186",
+    appId: "1:1074770307186:web:0f48b1238117ac23e7baeb"
+  }
 };
